0d5da9f1ee96c61031c6aedd5a8109d2dba26fde
Sage Elliott <wildtroll@gmail.com>
2015-09-18 13:50:25 -0700

    Add routes for profile CRUD

--- main.py
+++ main.py
@@ -2,10 +2,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+#list of profiles created by users
 @app.route('/')
-def Test():
-    pass
-    return "Hello, World"
+@app.route('/profiles/')
+def Profiles():
+    return "List profiles"
+
+
+# Create a new profile
+@app.route('/profile/new')
+def NewProfile():
+    return "Create a new profile"
+
+
+# Edit an exsisting profile
+#<int:profile_id>
+@app.route('/profile/1/edit/', methods=['GET', 'POST'])
+def EditProfile():
+    return "Edit a profile"
+
+
+# Delete an exsisting profile
+#<int:profile_id>
+@app.route('/profile/1/delete/', methods=['GET', 'POST'])
+def DeleteProfile():
+    return "Delete a profile"
+
+
 
 if __name__ == '__main__':
     app.debug = True
