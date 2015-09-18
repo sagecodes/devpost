8079b2edd88baf6afd4c290396dc73da55c64aa5
Sage Elliott <wildtroll@gmail.com>
2015-09-17 20:40:28 -0700

    create profile class and table in database_setup.
    create columns:
    - name
    - email
    - picture
    - github
    - twitter

--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,17 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Profile(Base):
+
+      __tablename__ = 'profile'
+
+  id = Column(Integer, primary_key = True)
+  name = Column(String(250), nullable = False)
+  email = Column(String(250), nullable = False)
+  picture = Column(String(250))
+  github = Column(String(250))
+  twitter = Column(String(250))
+
 
 engine = create_engine(
   'sqlite:///devpost.db')
