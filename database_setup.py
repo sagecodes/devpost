d0811a26791a4d9aff4d1289de0d52dc90d52e1e
Sage Elliott <wildtroll@gmail.com>
2015-09-17 18:03:18 -0700

    set create engine and create devpost.db

--- database_setup.py
+++ database_setup.py
@@ -6,3 +6,9 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+
+engine = create_engine(
+  'sqlite:///devpost.db')
+
+Base.metadata.create_all(engine)
