26d05d5495350573e2ad8fbc2a9518597ca39c74
Sage Elliott <wildtroll@gmail.com>
2015-09-26 15:01:56 -0700

    feat: login route creates state variable to prevent cross site forgery

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Authentication and Authorization
+#=======================================================================
+
+@app.route('/login/')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return "The current session state is %s" %login_session['state']
+
+
 # Profile operations
 #=======================================================================
 
@@ -151,5 +161,6 @@ def deleteProject(profile_id, project_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 3000)
