3f22cf8947b22e6dd1c007c99a58abd086a45a53
Sage Elliott <wildtroll@gmail.com>
2015-09-26 15:10:43 -0700

    Feat: login template
    - create templates/login.html
    - import google api script
    - create google sign in button
    - render template in showLogin function

--- main.py
+++ main.py
@@ -25,9 +25,10 @@ session = DBSession()
 
 @app.route('/login/')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                                                    for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" %login_session['state']
+    return render_template('login.html')
 
 
 # Profile operations
