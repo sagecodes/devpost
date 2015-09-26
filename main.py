251b72650e879b9885d7c1e5bc3aeb30f10e3cfd
Sage Elliott <wildtroll@gmail.com>
2015-09-26 16:26:08 -0700

    Feat: logout google session
    - create gdisconnect function and route in main.py
    - diconnect current connected user
    - delete all stored login_session saved data
    - send success response
    - if token invalid, let user know was unable to revoke

--- main.py
+++ main.py
@@ -132,6 +132,39 @@ def gconnect():
     return output
 
 
+@app.route("/gdisconnect/")
+def gdisconnect():
+    #only disconnect a connected user.
+    credentials = login_session.get('credentials')
+    if credentials is None:
+        response = make_response(json.dumps("Current user not connected."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    access_token = login_session.get('credentials')
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        # Reset the user's sesson.
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        # For whatever reason, the given token was invalid.
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
 # Profile operations
 #=======================================================================
 
