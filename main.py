6311240e94d04b19942ac672a77c3f27013aa4f8
Sage Elliott <wildtroll@gmail.com>
2015-09-28 11:25:33 -0700

    Feat: Gdisconnect now redirects to showProfiles and flashes success message on logout

--- main.py
+++ main.py
@@ -153,10 +153,10 @@ def gdisconnect():
         del login_session['username']
         del login_session['email']
         del login_session['picture']
-
         response = make_response(json.dumps('Successfully disconnected.'), 200)
         response.headers['Content-Type'] = 'application/json'
-        return response
+        flash("You have been successfully logged out")
+        return redirect(url_for('showProfiles'))
     else:
         # For whatever reason, the given token was invalid.
         response = make_response(
